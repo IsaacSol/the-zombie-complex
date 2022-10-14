edc2560da852387ad5b44c308426eb5d4bbb16c9
XavierJ69 <63133001+XavierJ69@users.noreply.github.com>
2022-10-13 20:17:34 -0500

    added classes for monsters, items, allies, and settings

---
+++ scripts/items.js
@@ -0,0 +1,12 @@
+export class Items {
+    constructor() {
+        this.name = name;
+        this.description = description;
+        this.effect = effect;
+        this.icon = icon;
+        this.pickUpSound = pickUpSound;
+        this.useSound = useSound;
+        this.type = type;
+        this.animFrames = animFrames;
+    }
+}
---
+++ scripts/monsters.js
@@ -0,0 +1,16 @@
+export class Monsters {
+    constructor() {
+        this.name = name;
+        this.hp = hp;
+        this.speed = speed;
+        this.atkSpeed = atkSpeed;
+        this.dmg = dmg;
+        this.range = range;
+        this.atkTypes = atkTypes;
+        this.icon = icon;
+        this.description = description;
+        this.animFrames = animFrames;
+        this.sounds = sounds;
+        this.soundRange = soundRange
+    }
+}
