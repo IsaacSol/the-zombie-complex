1d1b1a5a6879a2c2ee64ce485304e3c0e0afe03e
IsaacSol <soloisap001@gmail.com>
2022-10-13 20:00:40 -0500

    Started on weapon class, added to gamePage

---
+++ scripts/weapon.js
@@ -0,0 +1,10 @@
+export class Weapon {
+    constructor(name, description, damage, attackSpeed, attackRange, type) {
+        this.name = name; // Name of the weapon
+        this.description = description; // Description of the weapon
+        this.damage = damage; // Damage the weapon inflicts
+        this.attackSpeed = attackSpeed; // How 
+        this.attackRange = attackRange;
+        this.type = type;
+    }
+}
